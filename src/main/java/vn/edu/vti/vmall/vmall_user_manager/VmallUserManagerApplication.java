760071cdb46daec9c54eac0c6709a1d8bf8343fb
tunk <nguyenkhactu1996@gmail.com>
2024-06-03 22:16:46 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmall_user_manager/VmallUserManagerApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_user_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmallUserManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallUserManagerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/edu/vti/vmall/vmall_user_manager/VmallUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_user_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallUserManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
