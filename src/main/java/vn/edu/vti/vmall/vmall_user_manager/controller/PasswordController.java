038b2e1a90ab9a050fe9080bc555933369954edf
tunk <nguyenkhactu1996@gmail.com>
2024-06-12 20:38:42 +0700

    Add change password controller

---
+++ src/main/java/vn/edu/vti/vmall/vmall_user_manager/controller/PasswordController.java
@@ -0,0 +1,16 @@
+package vn.edu.vti.vmall.vmall_user_manager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.edu.vti.vmall.api.common.response.ApiResponse;
+
+@RestController
+@RequestMapping(value = "/api/v1/password")
+public class PasswordController {
+  @PutMapping
+  public ResponseEntity<ApiResponse<String>> changePassword(){
+    return ResponseEntity.ok(ApiResponse.success("Change password"));
+  }
+}
