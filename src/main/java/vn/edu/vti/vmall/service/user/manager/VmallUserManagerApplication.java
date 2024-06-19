7883c97c6a98cd42142ed205ae516f22bc3d841a
tunk <nguyenkhactu1996@gmail.com>
2024-06-19 16:06:07 +0700

    Get user info internal api

--- src/main/java/vn/edu/vti/vmall/service/user/manager/VmallUserManagerApplication.java
+++ src/main/java/vn/edu/vti/vmall/service/user/manager/VmallUserManagerApplication.java
@@ -12,16 +12,14 @@ import vn.edu.vti.vmall.security.config.JWTTokenProperties;
 @SpringBootApplication
 @EnableExceptionHandler
 @EnableSecurityCommon
-@RequiredArgsConstructor
 @Slf4j
 public class VmallUserManagerApplication implements CommandLineRunner {
-  private final JWTTokenProperties jwtTokenProperties;
   public static void main(String[] args) {
     SpringApplication.run(VmallUserManagerApplication.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
-    log.info("JWT KEY: [{}]", jwtTokenProperties.getKey());
+
   }
 }
